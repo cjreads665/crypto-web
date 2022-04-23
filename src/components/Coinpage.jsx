8c06a01dfe94396ff9109d2d69321ae58294d81d
cjreads665 <shahid.alam57@gmail.com>
2022-04-23 23:27:25 +0530

    testing3

--- src/components/Coinpage.jsx
+++ src/components/Coinpage.jsx
@@ -40,7 +40,7 @@ variables
 
 	return ( 
 		<div>
-	{/*{loading?<div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+	{loading?<div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
 	<div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
 	<h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
 	<p className="w-1/3 text-center text-white">This may take a few seconds, please don't close this page.</p>
@@ -58,7 +58,7 @@ variables
 					>{coin?.market_data.price_change_percentage_1h_in_currency[currency].toFixed(2)}%</span></h2>
 
 					<p className='px-4 text-center'>
-						{ReactHtmlParser(coin?.description.en.split('. ')[0])}
+						{/*ReactHtmlParser(coin?.description.en.split('. ')[0])*/}
 					</p>
 
 					<div className={styles.marketData}>
@@ -69,7 +69,7 @@ variables
 
 			<CoinInfo coin={coin} />
 		</div> 
-	}*/}
+	}
 
 		</div>
 		
